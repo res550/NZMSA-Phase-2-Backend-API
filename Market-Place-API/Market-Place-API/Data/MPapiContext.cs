778c8b57c7e0bea82c0d0eab2f3b9f66e3be0593
res550 <37566536+res550@users.noreply.github.com>
2018-11-20 17:18:31 +1300

    Building Api
    Building Api

---
+++ Market-Place-API/Market-Place-API/Data/MPapiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MPapi.Models
+{
+    public class MPapiContext : DbContext
+    {
+        public MPapiContext (DbContextOptions<MPapiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MPapi.Models.ListingItem> ListingItem { get; set; }
+    }
+}
---
+++ Market-Place-API/Market-Place-API/Models/ListingItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MPapi.Models
+{
+    public class ListingItem
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public string Price { get; set; }
+        public string Uploaded { get; set; }
+        public string email { get; set; }
+        public string Seller { get; set; }
+    }
+}
